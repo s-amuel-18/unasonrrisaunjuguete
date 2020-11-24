5360be3ccb3613dcf6fef5c19500fae30066cb90
Samuel Graterol <samuelgraterol12@gmail.com>
2020-11-24 15:01:08 -0400

    se agrego modal de formulario para las cartas de los niños y se programo para agregar los datos del mismo  a la base de datos, se comenzo a maquetar el modal del padrino

--- public/index.js
+++ public/index.js
@@ -7,6 +7,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var c = console.log;
 var formPadrino = document.querySelector("#formPadrino");
 var formLogIn = document.querySelector("#logIn");
+// const verificadorUser = false
 
 var addAvatar = function addAvatar(user) {
   var img = document.querySelector(".sesion-avatar");
@@ -17,10 +18,24 @@ var addAvatar = function addAvatar(user) {
   }
 };
 
+var progressBar = function progressBar(porcentaje) {
+  return "\n  <div class=\"progress\">\n    <div class=\"progress-bar my-3\" role=\"progressbar\" style=\"width: " + porcentaje + "%;\" aria-valuenow=\"25\" aria-valuemin=\"0\" aria-valuemax=\"100\">" + porcentaje + "%</div>\n  </div>\n  ";
+};
+
+var notificattion = function notificattion(selector, message, alert) {
+
+  selector.innerHTML += "\n  <div id=\"notification-alert\" class=\"failedLogin alert alert-" + alert + " my-3\" role=\"alert\">\n    " + message + "\n  </div>\n  ";
+
+  setTimeout(function () {
+    document.querySelector("#notification-alert").remove();
+  }, 8000);
+};
+
 var verifyUser = function verifyUser() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user !== null) {
-      document.body.innerHTML = "\n        <div class=\"menuNavegation bg-white fixed-top shadow-sm\">\n          <nav class=\"navbar navbar-expand-lg navbar-light bg-white font-f-title\">\n            <div class=\"container\">\n              <a class=\"navbar-brand\" href=\"#inicio\">\n                <img class=\"menuNavegation-logo preloaderImage-img\" data-img=\"./img/Logo NAV.png\" alt=\"\"/>\n              </a>\n              <div class=\"ml-auto d-flex\">\n                <img class=\"sesion-avatar  mr-3\">\n                <a href=\"#\" class=\"d-flex justify-content-center align-items-center signOut nav-link p-0\"> salir</a>\n              </div>\n            </div>\n          </nav>\n        </div>\n        <header>\n          <div class=\"headerSesion py-5 container \">\n            <img src=\"./img/ICONO DE USUARIO SIN FOTO.png\" alt=\"\" class=\"headerSesion-avatarHeader m-auto d-block\">\n            <h1 class=\"py-3 text-center text-verde\">" + (user.displayName !== null ? user.displayName : "Bienvenido") + "</h1>\n            <div class=\"headerSesion-contentButtons m-auto  py-4\">\n              <div class=\"row\">\n                <div class=\"p-2  col-sm-12 col-md-6 \"><button class=\"m-auto btn btn-block btn-buttonForm br-25  a-title \" data-toggle=\"modal\" data-target=\"#staticBackdrop\">ni\xF1o</button></div>\n                <div class=\"p-2  col-sm-12 col-md-6 \"><button class=\"m-auto btn btn-block btn-azul br-25  a-title \">padrino</button></div> \n              </div>\n            </div>\n          </div>\n        </header>\n\n\n        <div class=\"modal fade\" id=\"staticBackdrop\" data-backdrop=\"static\" data-keyboard=\"false\" tabindex=\"-1\" aria-labelledby=\"staticBackdropLabel\" aria-hidden=\"true\">\n          <div class=\"modal-dialog modal-dialog-centered\">\n            <div class=\"modal-content\">\n              <div class=\"modal-header\">\n                <h5 class=\"modal-title\" id=\"staticBackdropLabel\">Modal title</h5>\n                <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n                  <span aria-hidden=\"true\">&times;</span>\n                </button>\n              </div>\n              <div class=\"modal-body\">\n                ...\n              </div>\n              <div class=\"modal-footer\">\n                <button type=\"button\" class=\"br-25 btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n                <button type=\"button\" class=\"br-25 btn btn-primary\">Understood</button>\n              </div>\n            </div>\n          </div>\n        </div>\n      ";
+      document.body.innerHTML = "\n        <div class=\"menuNavegation bg-white fixed-top shadow-sm\">\n          <nav class=\"navbar navbar-expand-lg navbar-light bg-white font-f-title\">\n            <div class=\"container\">\n              <a class=\"navbar-brand\" href=\"#inicio\">\n                <img class=\"menuNavegation-logo preloaderImage-img\" data-img=\"./img/Logo NAV.png\" alt=\"\"/>\n              </a>\n              <div class=\"ml-auto d-flex\">\n                <img class=\"sesion-avatar  mr-3\">\n                <a href=\"#\" class=\"d-flex justify-content-center align-items-center signOut nav-link p-0\"> salir</a>\n              </div>\n            </div>\n          </nav>\n        </div>\n        <header>\n          <div class=\"headerSesion py-5 container \">\n            <img src=\"./img/ICONO DE USUARIO SIN FOTO.png\" alt=\"\" class=\"headerSesion-avatarHeader m-auto d-block\">\n            <h1 class=\"py-3 text-center text-verde\">" + (user.displayName !== null ? user.displayName : "Bienvenido") + "</h1>\n            <div class=\"headerSesion-contentButtons m-auto  py-4\">\n              <div class=\"row\">\n                <div class=\"p-2  col-sm-12 col-md-6 \"><button class=\"m-auto btn btn-block btn-buttonForm br-25  a-title \" data-toggle=\"modal\" data-target=\"#staticBackdrop\">ni\xF1o</button></div>\n                <div class=\"p-2  col-sm-12 col-md-6 \"><button class=\"m-auto btn btn-block btn-azul br-25  a-title \">padrino</button></div> \n              </div>\n            </div>\n          </div>\n        </header>\n\n\n        <div class=\"modal fade\" id=\"staticBackdrop\" data-backdrop=\"static\" data-keyboard=\"false\" tabindex=\"-1\"\n        aria-labelledby=\"staticBackdropLabel\" aria-hidden=\"true\">\n        <div class=\"modal-dialog modal-dialog-centered\">\n          <div class=\"modal-content modal-notification\">\n            <form id=\"formInfoBoy\">\n              <div class=\"modal-header\">\n                <h5 class=\"modal-title\" id=\"staticBackdropLabel\">Modal title</h5>\n                <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n                  <span aria-hidden=\"true\">&times;</span>\n                </button>\n              </div>\n              <div class=\"modal-body px-4\">\n                <div class=\"row\">\n                  <div class=\"col-lg-4 col-md-12 p-2 \">\n                    <div class=\"modalNinio-contentImg  m-auto\">\n                      <img src=\"./img/ICONO DE USUARIO SIN FOTO.png\" alt=\"\" class=\"modalNinio-img m-auto mb-3 d-block\" id=\"imagenNinio\">\n                      <label for=\"addFoto\" class=\"mt-3 btn-azul btn btn-block br-25 p-1\">subir foto <img\n                          class=\"h-15px ml-2 \" src=\"./img/ICONO SUBIR FOTO.svg\" alt=\"\"></label>\n                      <input type=\"file\" name=\"fotoNinio\" id=\"addFoto\" class=\"d-none\">\n                    </div>\n                  </div>\n                  <div class=\"col-lg-8 col-md-12  p-2\">\n                    <div class=\"modalNinio-contentInputs m-auto\">\n                      <div class=\"form-group\">\n                        <input name=\"names\" class=\"form-control br-25\" type=\"text\" name=\"nombre\" placeholder=\"nombre y apellido\"\n                          required=\"required\" />\n                      </div>\n\n                      <div class=\"form-group\">\n                        <textarea class=\"form-control br-25\" name=\"textArea\" rows=\"5\" required placeholder=\"Ingreza tu carta Navide\xF1a\"></textarea>\n                      </div>\n                      <input type=\"hidden\" id=\"idNinio\" name=\"idNinio\">\n                    </div>\n                  </div>\n                </div>\n              </div>\n              <div class=\"modal-footer\">\n                <button type=\"button\" class=\"p-2 px-4 br-25 btn btn-outline-secondary\"\n                  data-dismiss=\"modal\">Cancelar</button>\n                <input type=\"submit\" class=\"p-2 px-4  btn-azul br-25 btn btn-primary\" value=\"Ingrezar\">\n              </div>\n            </form>\n          </div>\n        </div>\n      </div>\n      <div class=\"modal fade\" id=\"modalprogressBar\" style=\"display:none; background-color: rgba(0,0,0,.2);\">\n        <div class=\"modal-dialog modal-md\">\n          <div class=\"modal-content p-3\">\n\n          <div class=\"progress\">\n            <div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"25\" aria-valuemin=\"0\" aria-valuemax=\"100\" id=\"progressbar\"></div>\n          </div>\n        </div>\n      </div>\n      ";
+      document.body.classList.remove("loading");
       document.addEventListener("click", function (e) {
         if (e.target.matches(".signOut")) {
           location.reload();
@@ -29,7 +44,74 @@ var verifyUser = function verifyUser() {
           });
         }
       });
+
+      document.querySelector("#formInfoBoy").addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        var names = e.target.names;
+        var idHidden = e.target.idNinio;
+        var carta = e.target.textArea;
+        var fotoNinio = e.target.fotoNinio;
+        var db = firebase.database();
+        var niniosRef = db.ref().child("ni\xF1os-users");
+        var storage = firebase.storage();
+        var bucket = storage.ref();
+        var imgRef = bucket.child("img-ni\xF1os");
+        var modalProgress = document.querySelector("#modalprogressBar");
+        var progressbar = document.querySelector("#progressbar");
+
+        // const 
+        if (names.value === "" || carta.value === "" || fotoNinio.value === "") {
+          // c(names.value ,idHidden.value , carta.value, fotoNinio.value === ``)
+          notificattion(e.target, "por favor ingreza todos los campos de formulario, la foto es requerida para el ingrezo de tu carta navide\xF1a", "danger");
+        }
+
+        var id = idHidden.value || niniosRef.push().key;
+
+        var formdata = new FormData(e.target);
+
+        var uploadTask = imgRef.child(formdata.get("fotoNinio").name).put(formdata.get("fotoNinio"));
+        uploadTask.on("stated_changed", function (data) {
+          var counter = Math.floor(data.bytesTransferred / data.totalBytes * 100);
+          // c(counter)
+          if (counter < 100) {
+            modalProgress.classList.add("show");
+            modalProgress.style.display = "block";
+            progressbar.style.width = counter + "%";
+            progressbar.textContent = counter + "%";
+          } else {
+            modalProgress.classList.remove("show");
+            modalProgress.style.display = "none";
+            fotoNinio.innerHTML = "";
+            e.target.reset();
+            notificattion(e.target, "se ah registrado correctamente ", "success");
+            document.querySelector("#imagenNinio").src = "./img/ICONO DE USUARIO SIN FOTO.png";
+          }
+        }, function (err) {}, function () {
+          var fileRef = imgRef.child(formdata.get("fotoNinio").name);
+
+          fileRef.getDownloadURL().then(function (url) {
+            var datanaNinios = {
+              names: names.value,
+              carta: carta.value,
+              padrino: false,
+              id: id,
+              photoURL: url
+            };
+            var updateData = {};
+            updateData["/" + id] = datanaNinios;
+            niniosRef.update(updateData);
+          });
+        });
+      });
+
+      document.querySelector("#addFoto").addEventListener("change", function (e) {
+        var urlImage = URL.createObjectURL(e.target.files[0]);
+        document.querySelector("#imagenNinio").src = urlImage;
+      });
       addAvatar(user);
+    } else {
+      document.body.classList.remove("loading");
     }
     preloaderImage(".preloaderImage-img");
   });
@@ -68,10 +150,9 @@ var Autentication = function () {
     key: "autEmailPass",
     value: function autEmailPass(email, pass) {
       // c(email, pass)
-      firebase.auth().signInWithEmailAndPassword(email.value, pass.value).then(function (response) {
-        c(response, " ok");
-      }).catch(function (err) {
-        formLogIn.innerHTML += "\n      <div class=\"failedLogin alert alert-danger my-3\" role=\"alert\">\n        " + err.message + "\n      </div>\n      ";
+      firebase.auth().signInWithEmailAndPassword(email.value, pass.value).then(function (response) {}).catch(function (err) {
+        // formLogIn.innerHTML += notificattion(err.message, `danger`)
+
       });
     }
   }, {
@@ -83,6 +164,8 @@ var Autentication = function () {
           pa: "sapo"
         });
         c(result.user);
+      }).catch(function (err) {
+        // formPadrino.innerHTML += notificattion(err.message, `danger`)
       });
     }
   }, {
@@ -93,11 +176,8 @@ var Autentication = function () {
       firebase.auth().signInWithPopup(provider).then(function (result) {
         c(result);
       }).catch(function (err) {
-        formPadrino.innerHTML += "\n      <div class=\"failedLogin alert alert-danger my-3\" role=\"alert\">\n        hubo un error puedes ingrezar registrando tus datos en el formulario\n      </div>\n      ";
+        // formPadrino.innerHTML += notificattion(err.message , `danger`)
 
-        setTimeout(function () {
-          document.querySelector(".failedLogin").remove();
-        }, 8000);
       });
     }
   }]);
@@ -108,9 +188,8 @@ var Autentication = function () {
 window.addEventListener("load", function (e) {
   verifyUser();
   preloaderImage(".preloaderImage-img");
-  setTimeout(function () {
-    document.body.classList.remove("loading");
-  }, 3000);
+  // setTimeout(() => {
+  // },3000)
 });
 
 document.addEventListener("click", function (e) {
@@ -132,7 +211,6 @@ formPadrino.addEventListener("submit", function (e) {
 
 formLogIn.addEventListener("submit", function (e) {
   e.preventDefault();
-  c("submit");
   var auth = new Autentication();
   auth.autEmailPass(e.target.email, e.target.password);
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const c = console.log
 const formPadrino = document.querySelector(`#formPadrino`)
 const formLogIn = document.querySelector(`#logIn`)
-
+// const verificadorUser = false
 
 const addAvatar = (user) => {
   let img = document.querySelector(`.sesion-avatar`)
@@ -10,7 +10,27 @@ const addAvatar = (user) => {
   }else{
     img.src = `./img/ICONO DE USUARIO SIN FOTO.png`
   }
-  
+}
+
+const progressBar = (porcentaje) => {
+  return `
+  <div class="progress">
+    <div class="progress-bar my-3" role="progressbar" style="width: ${porcentaje}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${porcentaje}%</div>
+  </div>
+  `
+}
+
+const notificattion = (selector, message, alert) => {
+
+  selector.innerHTML += `
+  <div id="notification-alert" class="failedLogin alert alert-${alert} my-3" role="alert">
+    ${message}
+  </div>
+  `
+
+  setTimeout(() => {
+    document.querySelector(`#notification-alert`).remove()
+  }, 8000)
 }
 
 const verifyUser = () => {
@@ -44,26 +64,62 @@ const verifyUser = () => {
         </header>
 
 
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
+        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content modal-notification">
+            <form id="formInfoBoy">
               <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body">
-                ...
+              <div class="modal-body px-4">
+                <div class="row">
+                  <div class="col-lg-4 col-md-12 p-2 ">
+                    <div class="modalNinio-contentImg  m-auto">
+                      <img src="./img/ICONO DE USUARIO SIN FOTO.png" alt="" class="modalNinio-img m-auto mb-3 d-block" id="imagenNinio">
+                      <label for="addFoto" class="mt-3 btn-azul btn btn-block br-25 p-1">subir foto <img
+                          class="h-15px ml-2 " src="./img/ICONO SUBIR FOTO.svg" alt=""></label>
+                      <input type="file" name="fotoNinio" id="addFoto" class="d-none">
+                    </div>
+                  </div>
+                  <div class="col-lg-8 col-md-12  p-2">
+                    <div class="modalNinio-contentInputs m-auto">
+                      <div class="form-group">
+                        <input name="names" class="form-control br-25" type="text" name="nombre" placeholder="nombre y apellido"
+                          required="required" />
+                      </div>
+
+                      <div class="form-group">
+                        <textarea class="form-control br-25" name="textArea" rows="5" required placeholder="Ingreza tu carta Navideña"></textarea>
+                      </div>
+                      <input type="hidden" id="idNinio" name="idNinio">
+                    </div>
+                  </div>
+                </div>
               </div>
               <div class="modal-footer">
-                <button type="button" class="br-25 btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="br-25 btn btn-primary">Understood</button>
+                <button type="button" class="p-2 px-4 br-25 btn btn-outline-secondary"
+                  data-dismiss="modal">Cancelar</button>
+                <input type="submit" class="p-2 px-4  btn-azul br-25 btn btn-primary" value="Ingrezar">
               </div>
-            </div>
+            </form>
           </div>
         </div>
+      </div>
+      <div class="modal fade" id="modalprogressBar" style="display:none; background-color: rgba(0,0,0,.2);">
+        <div class="modal-dialog modal-md">
+          <div class="modal-content p-3">
+
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" id="progressbar"></div>
+          </div>
+        </div>
+      </div>
       `
+      document.body.classList.remove(`loading`)
       document.addEventListener(`click`, e => {
         if(e.target.matches(`.signOut`)){
           location.reload()
@@ -71,9 +127,83 @@ const verifyUser = () => {
             .then( () =>  e.target.remove())
         }
       })
+
+      document.querySelector(`#formInfoBoy`).addEventListener(`submit`, e => {
+        e.preventDefault()
+
+        const names = e.target.names
+        const idHidden = e.target.idNinio
+        const carta = e.target.textArea
+        const fotoNinio = e.target.fotoNinio
+        const db = firebase.database()
+        const niniosRef = db.ref().child(`niños-users`)
+        const storage = firebase.storage()
+        const bucket = storage.ref()
+        const imgRef = bucket.child(`img-niños`)
+        const modalProgress = document.querySelector(`#modalprogressBar`)
+        const progressbar = document.querySelector(`#progressbar`)
+
+        // const 
+        if(names.value === `` || carta.value === `` || fotoNinio.value === ``){
+          // c(names.value ,idHidden.value , carta.value, fotoNinio.value === ``)
+          notificattion(e.target,`por favor ingreza todos los campos de formulario, la foto es requerida para el ingrezo de tu carta navideña`, `danger`)
+        }
+
+        let id = idHidden.value || niniosRef.push().key
+
+        let formdata = new FormData(e.target)
+
+        let uploadTask = imgRef.child(formdata.get(`fotoNinio`).name).put(formdata.get(`fotoNinio`))
+        uploadTask.on(`stated_changed`, data => {
+          let counter = Math.floor((data.bytesTransferred / data.totalBytes) * 100)
+          // c(counter)
+          if(counter < 100){
+            modalProgress.classList.add(`show`)
+            modalProgress.style.display = `block`
+            progressbar.style.width = `${counter}%`
+            progressbar.textContent = counter + `%`
+          }else{
+            modalProgress.classList.remove(`show`)
+            modalProgress.style.display = `none`
+            fotoNinio.innerHTML = ``
+            e.target.reset()
+            notificattion(e.target,`se ah registrado correctamente `, `success`)
+            document.querySelector(`#imagenNinio`).src = `./img/ICONO DE USUARIO SIN FOTO.png`
+
+          }
+
+        }, err => {
+
+        }, ( ) => {
+          let fileRef = imgRef.child(formdata.get(`fotoNinio`).name)
+
+          fileRef.getDownloadURL()
+            .then(url => {
+              let datanaNinios = {
+                names:names.value,
+                carta: carta.value,
+                padrino: false,
+                id: id,
+                photoURL: url
+              }
+              let updateData = {}
+              updateData[`/${id}`] = datanaNinios
+              niniosRef.update(updateData)
+            })
+        })
+        
+       
+      })
+
+      document.querySelector(`#addFoto`).addEventListener(`change`, e => {
+        let urlImage = URL.createObjectURL(e.target.files[0])
+        document.querySelector(`#imagenNinio`).src = urlImage
+      })
       addAvatar(user)
+    }else {
+      document.body.classList.remove(`loading`)
     }
-    preloaderImage(`.preloaderImage-img`)
+    preloaderImage(`.preloaderImage-img`) 
   })
 
 
@@ -111,16 +241,15 @@ class Autentication {
       pass.value,
     )
     .then(response => {
-      c(response, ` ok`)
     })
     .catch(err => {
-      formLogIn.innerHTML += `
-      <div class="failedLogin alert alert-danger my-3" role="alert">
-        ${err.message}
-      </div>
-      `
+      // formLogIn.innerHTML += notificattion(err.message, `danger`)
+      
     })
   }
+
+
+
   crearCuentaEmailPass(email, pass, names){
     firebase
       .auth().createUserWithEmailAndPassword(email, pass)
@@ -130,6 +259,9 @@ class Autentication {
             pa: `sapo`
           })
           c(result.user)
+        })
+        .catch(err => {
+          // formPadrino.innerHTML += notificattion(err.message, `danger`)
         })
   }
 
@@ -141,15 +273,8 @@ class Autentication {
       c(result)
     })
     .catch(err => {
-      formPadrino.innerHTML += `
-      <div class="failedLogin alert alert-danger my-3" role="alert">
-        hubo un error puedes ingrezar registrando tus datos en el formulario
-      </div>
-      `
+      // formPadrino.innerHTML += notificattion(err.message , `danger`)
 
-      setTimeout(() => {
-        document.querySelector(`.failedLogin`).remove()
-      },8000)
     })
   }
 }
@@ -161,9 +286,9 @@ class Autentication {
 window.addEventListener(`load`, e => {
     verifyUser()
     preloaderImage(`.preloaderImage-img`)
-    setTimeout(() => {
-      document.body.classList.remove(`loading`)
-    },3000)
+    // setTimeout(() => {
+    // },3000)
+
 })
 
 
@@ -183,11 +308,11 @@ formPadrino.addEventListener(`submit`, e => {
   const passWord = document.getElementById(`padrinoPass`)
   const auth = new Autentication()
   auth.crearCuentaEmailPass(email.value, passWord.value,name.value )
+
 }) 
 
 formLogIn.addEventListener(`submit`, e => {
   e.preventDefault()
-  c(`submit`)
   let auth = new Autentication 
   auth.autEmailPass(e.target.email,e.target.password)
 })
